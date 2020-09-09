75882414123a4bbe367d5e0296077d6bb565d081
QuadieZ <62594692+QuadieZ@users.noreply.github.com>
2020-09-09 23:03:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi')
+var modal = document.getElementById('myModal');
+
+function modalPop() {
+    modal.style.display = "flex";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
